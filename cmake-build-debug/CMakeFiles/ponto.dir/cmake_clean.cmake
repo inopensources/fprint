11771b4790fd551d4cdecc645d4d4e795e9152c1
fernandesleticia <leticiafernandes777@gmail.com>
2018-09-05 17:47:54 -0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/ponto.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ponto.dir/main.c.o"
+  "ponto.pdb"
+  "ponto"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ponto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
